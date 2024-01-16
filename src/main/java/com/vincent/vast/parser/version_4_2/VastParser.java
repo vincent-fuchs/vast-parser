003fad3047b7511fda6c63a2f64c21239e2d32d2
Vincent Fuchs <vincent.fuchs@tritondigital.com>
2024-01-16 10:08:46 -0500

    trying different approach

--- src/main/java/com/vincent/vast/parser/version_4_2/VastParser.java
+++ src/main/java/com/vincent/vast/parser/version_4_2/VastParser.java
@@ -2,25 +2,42 @@ package com.vincent.vast.parser.version_4_2;
 
 import com.vincent.vast.version_4_2.VAST;
 import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
-import javax.xml.transform.stream.StreamSource;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.io.StringReader;
 
 public class VastParser {
 
-    public static CustomVast parseXml(String xmlString) {
+    public static CustomVast parseXml(String xmlString) throws ParserConfigurationException {
 
         try {
 
             JAXBContext jc = JAXBContext.newInstance(VAST.class);
             Unmarshaller u = jc.createUnmarshaller();
-            VAST result = u.unmarshal(new StreamSource(new StringReader(xmlString)), VAST.class).getValue();
 
-            return new CustomVast(result);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            dbf.setNamespaceAware(true);
+
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new InputSource(new StringReader(xmlString)));
+            JAXBElement<VAST> foo = u.unmarshal( doc, VAST.class);
+
+            return new CustomVast(foo.getValue());
 
         } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
             throw new RuntimeException(e);
         }
     }
--- src/test/java/com/vincent/vast/parser/version_4_2/VastParserTest.java
+++ src/test/java/com/vincent/vast/parser/version_4_2/VastParserTest.java
@@ -2,6 +2,7 @@ package com.vincent.vast.parser.version_4_2;
 
 import org.junit.jupiter.api.Test;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class VastParserTest {
 
     @Test
-    void withNamespaceInXml() throws IOException {
+    void withNamespaceInXml() throws IOException, ParserConfigurationException {
 
         Path path = Paths.get("src/test/resources/sample4_2.xml");
         String vastXmlContent = Files.lines(path).collect(Collectors.joining(System.lineSeparator()));
@@ -25,7 +26,7 @@ class VastParserTest {
     }
 
     @Test
-    void canParseAnXMlWithNoNamespace() throws IOException {
+    void canParseAnXMlWithNoNamespace() throws IOException, ParserConfigurationException {
 
         Path path = Paths.get("src/test/resources/sample4_2_noNamespace.xml");
         String vastXmlContent = Files.lines(path).collect(Collectors.joining(System.lineSeparator()));
